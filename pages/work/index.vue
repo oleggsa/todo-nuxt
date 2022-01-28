73b7b4d0b7e9b04e74d8a509de68369267af0359
Oleh Didyk <oleggsa@gmail.com>
2022-01-28 09:50:22 +0200

    added pages to work with

---
+++ pages/work/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Work Todos</h1>
+        <div class="todos__wrapper">
+            <TodoCard v-for="todo in 9" :key='todo'>{{todo}}</TodoCard>
+        </div>
+    </div>
+</template>
+
+<script>
+import TodoCard from '@/components/Todocard'
+    export default {
+        components: {
+            TodoCard
+        }
+    }
+</script>
+
+<style scoped>
+    .todos__wrapper {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: left;
+    }
+</style>
