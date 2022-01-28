1f6f18abe7ea477d271a9ac7663f845c87c05478
Oleh Didyk <oleggsa@gmail.com>
2022-01-28 09:49:38 +0200

    added todo card sample

---
+++ components/TodoCard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="todos__card">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    .todos__card {
+        min-width: 200px;
+        min-height: 200px;
+        margin: 20px;
+        box-shadow: 3px 3px 5px black;
+        background: blanchedalmond;
+        padding: 10px;
+        border-radius: 10px;
+    }
+</style>
