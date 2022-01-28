2ad58300447c2e2f6f4baec00031ebf7a691d2ec
Oleh Didyk <oleggsa@gmail.com>
2022-01-28 09:49:56 +0200

    builded structure

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <Navbar />
+        <AddTodo />
+        <main>
+            <div class="container">
+                <nuxt />
+            </div>
+        </main>
+    </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+import AddTodo from '@/components/AddTodo.vue'
+
+export default {
+    components: {
+        Navbar,
+        AddTodo
+    }
+}
+</script>
